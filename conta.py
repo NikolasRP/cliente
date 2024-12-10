fbfac86d99d83e61d788c4433801cdb7e5640b31
Nikolas R. Pires <nikipires1985@gmail.com>
2024-12-10 08:27:44 -0300

    Update conta.py

--- conta.py
+++ conta.py
@@ -3,3 +3,12 @@ class conta:
     self.saldo = 0
     self.numero = numero
     self.titular = titular
+
+def get_saldo(self):
+  return self._saldo
+
+def set_saldo(self, saldo):
+  if (saldo < 0):
+    print("o saldo não pode ser negativo")
+  else:
+    self._saldo = saldo
