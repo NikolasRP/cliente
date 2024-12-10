a5fdefd5a16f9e615126793be6f0dde5cac82082
Nikolas R. Pires <nikipires1985@gmail.com>
2024-12-10 08:24:11 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -3,3 +3,11 @@ def __init__(self, n, fone):
   self.nome = n
 
 self.telefone = fone
+
+#método get
+def get_nome(self):
+  return self.nome
+
+#método set
+def set_nome(self, nome):
+  self._nome = nome
