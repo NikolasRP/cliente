6815ca1fbc878f98ba5bae73458cdcb9f3be2104
Nikolas R. Pires <nikipires1985@gmail.com>
2024-12-10 08:21:51 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+class main:
+  pass
+
+print("Testando o projeto!")
+
+from cliente import cliente
+
+from conta import conta
+
+c1 = Cliente ("joão, numero: 112233-445566, conta.numero, "seu saldo: ", conta.saldo)
