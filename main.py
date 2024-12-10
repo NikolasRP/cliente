6cb8d9b865319038f5e981e8e59201dbdee6476b
Nikolas R. Pires <nikipires1985@gmail.com>
2024-12-10 08:44:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ from cliente import cliente
 from conta import conta
 
 c1 = Cliente ("joão, numero: 112233-445566, conta.numero, "seu saldo: ", conta.saldo)
+
+conta.deposita(100)
+conta.saque(50)
+conta.extrato(10)
